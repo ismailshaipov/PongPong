2130d31b98256609efb9c15d8be7bbf790ec1363
ismailshaipov <ismail_shaipov95@mail.ru>
2024-04-10 01:19:23 +0300

    Добавьте файлы проекта.

---
+++ Arcanoid/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Arcanoid
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(488, 585);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PongPong";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
---
+++ Arcanoid/Physics2DController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arcanoid
+{
+    class Physics2DController
+    {
+        // Константы для размеров клеток и значений карты
+        private const int CellSize = 20;
+        private const int MaxCellValue = 99;
+        private const int MinBrickValue = 10;
+
+        public bool IsCollide(Player player, MapController map, Label scoreLabel)
+        {
+            bool isColliding = false;
+
+            // Проверка столкновения с боковыми границами карты
+            if (player.ballX / CellSize + player.dirX > MapController.mapWidth - 1 || player.ballX / CellSize + player.dirX < 0)
+            {
+                player.dirX *= -1; // Обратное направление движения по оси X
+                isColliding = true;
+            }
+
+            // Проверка столкновения с верхней границей карты
+            if (player.ballY / CellSize + player.dirY < 0)
+            {
+                player.dirY *= -1; // Обратное направление движения по оси Y
+                isColliding = true;
+            }
+
+            // Получаем индексы ячейки на карте, в которой находится мяч
+            int cellX = player.ballX / CellSize;
+            int cellY = player.ballY / CellSize;
+
+            // Проверка столкновения с блоком на карте
+            if (map.map[cellY + player.dirY, cellX] != 0)
+            {
+                isColliding = true;
+
+                // Обработка разрушения блока
+                if (map.map[cellY + player.dirY, cellX] > MinBrickValue && map.map[cellY + player.dirY, cellX] < MaxCellValue)
+                {
+                    map.map[cellY + player.dirY, cellX] = 0; // Уничтожаем блок
+                    map.map[cellY + player.dirY, cellX - 1] = 0; // Уничтожаем блок слева
+                    player.score += 50; // Увеличиваем счет
+                    if (player.score % 200 == 0 && player.score > 0)
+                    {
+                        map.AddLine(); // Добавляем новую строку блоков
+                    }
+                }
+                else if (map.map[cellY + player.dirY, cellX] < MinBrickValue)
+                {
+                    map.map[cellY + player.dirY, cellX] = 0; // Уничтожаем блок
+                    map.map[cellY + player.dirY, cellX + 1] = 0; // Уничтожаем блок справа
+                    player.score += 50; // Увеличиваем счет
+                    if (player.score % 200 == 0 && player.score > 0)
+                    {
+                        map.AddLine(); // Добавляем новую строку блоков
+                    }
+                }
+                player.dirY *= -1; // Обратное направление движения по оси Y
+            }
+
+            // Проверка столкновения с блоком на карте по оси X
+            if (map.map[cellY, (player.ballX / CellSize) + player.dirX] != 0)
+            {
+                isColliding = true;
+
+                // Обработка разрушения блока
+                if (map.map[cellY, (player.ballX / CellSize) + player.dirX] > MinBrickValue && map.map[cellY + player.dirY, cellX] < MaxCellValue)
+                {
+                    map.map[cellY, (player.ballX / CellSize) + player.dirX] = 0; // Уничтожаем блок
+                    map.map[cellY, (player.ballX / CellSize) + player.dirX - 1] = 0; // Уничтожаем блок слева
+                    player.score += 50; // Увеличиваем счет
+                    if (player.score % 200 == 0 && player.score > 0)
+                    {
+                        map.AddLine(); // Добавляем новую строку блоков
+                    }
+                }
+                else if (map.map[cellY, (player.ballX / CellSize) + player.dirX] < MinBrickValue)
+                {
+                    map.map[cellY, (player.ballX / CellSize) + player.dirX] = 0; // Уничтожаем блок
+                    map.map[cellY, (player.ballX / CellSize) + player.dirX + 1] = 0; // Уничтожаем блок справа
+                    player.score += 50; // Увеличиваем счет
+                    if (player.score % 200 == 0 && player.score > 0)
+                    {
+                        map.AddLine(); // Добавляем новую строку блоков
+                    }
+                }
+                player.dirX *= -1; // Обратное направление движения по оси X
+            }
+
+            scoreLabel.Text = "Счет: " + player.score; // Обновление метки счета
+
+            return isColliding;
+        }
+    }
+}
